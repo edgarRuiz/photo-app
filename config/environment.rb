1c5a2adebf3354e4bf0d48848b74ee9d3ccc74a3
Edgar <ed.ruiz1106@gmail.com>
2016-10-31 01:28:39 +0000

    production email fix attempt 1

--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,20 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
-  :port => '587',
+  :port => 587,
   :authentication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
+
+# ActionMailer::Base.smtp_settings = {
+#   :user_name => 'your_sendgrid_username',
+#   :password => 'your_sendgrid_password',
+#   :domain => 'yourdomain.com',
+#   :address => 'smtp.sendgrid.net',
+#   :port => 587,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
+# }
